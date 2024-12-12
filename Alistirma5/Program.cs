721d552127dd88fb81fe3fff3539a44a58ad569d
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 13:58:14 +0300

    C# Alistirma 5 Yapıldı.

--- Alistirma5/Program.cs
+++ Alistirma5/Program.cs
@@ -10,6 +10,52 @@ namespace Alistirma5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*************** Araç Sigorta Ücreti Hesaplama ******************");
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            Console.WriteLine("Lütfen Araç Türünü Giriniz");
+            Console.WriteLine("1 - Otomobil ");
+            Console.WriteLine("2 - Motosiklet ");
+            Console.WriteLine("3 - Ticari ");
+            Console.Write("seciniz : ");
+            string AracTuru = Console.ReadLine();
+
+            Console.Clear();
+
+            
+            int Otomobil = 1000;
+            int Motor = 500;
+            int Ticari = 2000;
+            int toplam;
+            int yas = 0;
+            if (AracTuru == "1" || AracTuru == "2" || AracTuru == "3")
+            {
+                Console.WriteLine("Lütfen Yaşınızı Giriniz");
+                 yas = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Hatalı Bir Seçim Yaptınız");
+            }
+
+            if(AracTuru == "1")
+            {
+                toplam = Otomobil + yas * 20;
+
+                Console.WriteLine("Toplam Sigorta : " + toplam);
+            }
+            else if(AracTuru == "2")
+            {
+                toplam = Motor + yas * 10;
+
+                Console.WriteLine("Toplam Sigorta : " + toplam);
+            }
+            else if(AracTuru == "3")
+            {
+                toplam = Ticari + yas * 30;
+
+                Console.WriteLine("Toplam Sigorta : " + toplam);
+            }
         }
     }
 }
