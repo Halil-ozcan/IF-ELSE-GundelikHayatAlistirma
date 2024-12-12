bb40457f937ab2ff77584f8f1e53441b13da22db
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 13:30:19 +0300

    C# Alistirma 4 Yapıldı.

--- Alistirma4/Program.cs
+++ Alistirma4/Program.cs
@@ -10,6 +10,54 @@ namespace Alistirma4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("********************* Online Market Teslimat Süresi *********************");
+
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+
+            Console.WriteLine("Lütfen Siparis Tutarını Giriniz : ");
+            decimal tutar = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Lütfen teslimat Mesafesini Giriniz(km) : ");
+            decimal mesafe = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+
+            int teslimatUcreti = 10;
+            decimal ToplamTutar = 0;
+
+            if(mesafe > 0 && mesafe <= 10)
+            {
+                Console.WriteLine("Mesafeniz" + mesafe);
+                Console.WriteLine("Mesafeniz Kısa olduğundan Teslimat Ücreti Ücretsiz. " + " Ücretiniz " + tutar);
+            }
+            else if(mesafe > 10 && mesafe <=30)
+            {
+                Console.WriteLine("Mesafeniz" + mesafe);
+                ToplamTutar = tutar + teslimatUcreti;
+
+                Console.WriteLine("Teslimatınız 10 km den fazla olduğu için Teslimat Ücretiniz" + teslimatUcreti + "dir.");
+
+                Console.WriteLine("Toplam Tutarınız : " + ToplamTutar);
+            }
+            else
+            {
+                if(tutar < 200)
+                {
+                    Console.WriteLine("30 Km üstü Siparişlerde sipariş Tutarı en az 200 Tl Olmalıdır");
+                }
+                else
+                {
+                    teslimatUcreti = 30;
+
+                    Console.WriteLine("Mesafeniz : " + mesafe);
+                    ToplamTutar = tutar + teslimatUcreti;
+
+                    Console.WriteLine("Teslimatınız 30 km den fazla olduğu için Teslimat Ücretiniz " + teslimatUcreti + " Tl dir.");
+
+                    Console.WriteLine("Toplam Tutarınız : " + ToplamTutar);
+                }
+            }
         }
     }
 }
