d61529fed70cc85696bfa62f619f9f8fb63e6075
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 12:39:19 +0300

    C# Alistirma 2 Yapıldı

--- Alistirma2/Program.cs
+++ Alistirma2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace Alistirma2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*************** Kredi Kartı Ödeme Sistemi *****************");
+
+            Console.Write("Kredi Kartı Borcunuzu Giriniz : ");
+            decimal borc = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Borcunuz : " + borc);
+            Console.WriteLine("Ödenecek Turarı Giriniz : ");
+            decimal odeme = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+
+            decimal kalan = borc - odeme;
+
+            if (kalan == 0)
+            {
+                Console.WriteLine("Teşekkür ederiz Borcunuz Kalmadı.");
+            }
+            else if (odeme > borc / 2)
+            {
+                Console.WriteLine("Kalan Borcunuz : " + kalan + " Tl ");
+            }
+            else
+            {
+                Console.WriteLine("Kalan Borcunuz : " + kalan + " Tl ");
+                Console.WriteLine("Lütfen kalan borcunuzu en kısa sürede ödeyin");
+            }
+        
         }
     }
 }
