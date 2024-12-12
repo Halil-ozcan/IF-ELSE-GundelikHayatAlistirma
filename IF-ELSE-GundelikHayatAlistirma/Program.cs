75d49491e357cb28bf4cfc40a92d6a06a7ffa2bf
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 12:00:07 +0300

    C# Alistirma 1 Yapıldı

--- IF-ELSE-GundelikHayatAlistirma/Program.cs
+++ IF-ELSE-GundelikHayatAlistirma/Program.cs
@@ -10,6 +10,25 @@ namespace IF_ELSE_GundelikHayatAlistirma
     {
         static void Main(string[] args)
         {
+            Console.Write("Lütfen Yaşınızı Giriniz : ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            if(yas > 0 && yas<=12)
+            {
+                Console.WriteLine("Çocuk");
+            }
+            else if(yas > 13 && yas<=19)
+            {
+                Console.WriteLine("Genç");
+            }
+            else if(yas > 20 && yas<=64)
+            {
+                Console.WriteLine("Yetişkin");
+            }
+            else
+            {
+                Console.WriteLine("Yaşlı");
+            }
         }
     }
 }
